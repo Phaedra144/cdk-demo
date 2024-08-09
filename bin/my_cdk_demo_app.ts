2b6d58a7745e8ec3203594e468fc9ae1a9893613
Phaedra144 <bedi.szilvia@gmail.com>
2024-08-09 07:59:46 +0200

    My first cdk demo project

--- bin/my_cdk_demo_app.ts
+++ bin/my_cdk_demo_app.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { MyCdkDemoAppStack } from '../lib/my_cdk_demo_app-stack';
 
 const app = new cdk.App();
-new MyCdkDemoAppStack(app, 'MyCdkDemoAppStack', {
+new MyCdkDemoAppStack(app, "MyCdkDemoAppStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -15,7 +15,7 @@ new MyCdkDemoAppStack(app, 'MyCdkDemoAppStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: "232256076972", region: "eu-west-1" },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/my_cdk_demo_app-stack.ts
+++ lib/my_cdk_demo_app-stack.ts
@@ -1,5 +1,8 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { SqsDestination } from "aws-cdk-lib/aws-s3-notifications";
+import { Bucket, CfnBucket, EventType } from "aws-cdk-lib/aws-s3";
+import { Queue } from "aws-cdk-lib/aws-sqs";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class MyCdkDemoAppStack extends cdk.Stack {
@@ -12,5 +15,25 @@ export class MyCdkDemoAppStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'MyCdkDemoAppQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const level1S3Bucket = new CfnBucket(this, "MyFirstLevel1Bucket", {
+      versioningConfiguration: {
+        status: "Enabled",
+      },
+    });
+
+    const level2S3Bucket = new Bucket(this, "MyFirstLevel2Bucket", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      bucketName: "my-very-first-level2-bucket-name",
+      versioned: true,
+    });
+
+    const queue = new Queue(this, "MyQueue", {
+      queueName: "myQueue",
+    });
+
+    level2S3Bucket.addEventNotification(
+      EventType.OBJECT_CREATED,
+      new SqsDestination(queue)
+    );
   }
 }
